205f5518c2c501b0c892a5bc91eefc0104258811
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-03 02:36:17 +0530

    feat: Added express js and created root route

--- Server/src/App.ts
+++ Server/src/App.ts
@@ -1,1 +1,12 @@
-console.log("Hello Quizzers")
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express()
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, Dev Quizzer")
+})
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
