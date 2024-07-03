e58c6387ab12158331676d5a4e5e2a764b72f0b5
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-04 01:52:50 +0530

    refactor: Default category route to give only id name and image

--- Server/src/routes/CategoryRoutes.ts
+++ Server/src/routes/CategoryRoutes.ts
@@ -10,7 +10,7 @@ const CategoryRouter: Router = express.Router();
 
 CategoryRouter.get("/", async (req, res) => {
 	try {
-		const response = await Category.find();
+		const response = await Category.find({}, { id: 1, name: 1, image: 1 });
 		res.status(200).json({ results: response });
 	} catch (error) {
 		console.log("error: " + error);
@@ -25,7 +25,9 @@ CategoryRouter.post("/add", async (req: Request, res: Response) => {
 		for (const category of data) {
 			const isCategoryPresent = await Category.findOne({ name: category.name });
 			if (isCategoryPresent) continue;
-			newCategories.push(new Category({ id: await UniqueCategoryId(), name: category.name, image: category.image }));
+			newCategories.push(
+				new Category({ id: await UniqueCategoryId(), name: category.name, image: category.image })
+			);
 		}
 		const response = await Category.insertMany(newCategories);
 		console.log(newCategories.length + " Data Saved");
