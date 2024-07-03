398888f60908b70cf9849e96c50658c718027f0d
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-03 12:59:13 +0530

    pack: Refactored project with nodemon

--- Server/src/routes/AdminRoutes.ts
+++ Server/src/routes/AdminRoutes.ts
@@ -8,15 +8,14 @@ const AdminRouter: Router = express.Router();
 
 AdminRouter.post("/", async (req: Request, res: Response) => {
 	try {
-		const token = req.body;
-		console.log(req.body.token);
+		const token: String = req.body.token;
 		if (token === process.env.ADMIN_TOKEN) {
 			return res.status(200).json({ status: 200 });
 		}
 		return res.status(402).json({ status: 402, message: "Invalid Token" });
 	} catch (error) {
 		console.log(`Invalid Token : ${error}`);
-		res.status(500).json({ error: error });
+		return res.status(500).json({ error: error });
 	}
 });
 
@@ -26,10 +25,10 @@ AdminRouter.post("/ques-req", async (req: Request, res: Response) => {
 		const newReq = new QuestionRequest(data);
 		await newReq.save();
 		console.log(`Request sent successfully`);
-		res.status(200).json({ status: 200, message: `Request sent successfully` });
+		return res.status(200).json({ status: 200, message: `Request sent successfully` });
 	} catch (err) {
 		console.log(`Error in Sending Request : ${err}`);
-		res.status(500).json({ error: err });
+		return res.status(500).json({ error: err });
 	}
 });
 
