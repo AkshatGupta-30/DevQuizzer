32bd09caa41105c61a10104a9860ae182048b4ec
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-03 11:34:37 +0530

    Added body parser with express

--- Server/src/App.ts
+++ Server/src/App.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import DbConnect from './Database';
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 DbConnect();
 
 const app: Express = express()
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, Dev Quizzer")
