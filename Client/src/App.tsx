91b48dff3c30c0a71dcb88c2395efbc11012324e
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-03 20:48:52 +0530

    feat: Added footer

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
 
 const App = React.memo(() => {
 	return (
@@ -10,6 +11,7 @@ const App = React.memo(() => {
 				<Header/>
 				<Routes>
 				</Routes>
+				<Footer/>
 			</BrowserRouter>
 		</div>
 	);
