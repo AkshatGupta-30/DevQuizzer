3a59c6f638f527777180b003ffdb367292f12e72
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-08 18:15:49 +0530

    feat: Added question skeleton

--- Client/src/components/Questions/Questions.tsx
+++ Client/src/components/Questions/Questions.tsx
@@ -4,7 +4,20 @@ import "./Questions.scss";
 const Questions = memo(() => {
 	return (
 		<div className='questions'>
-			<div className='ques'></div>
+			<div className='name'>
+				<div className='number'>Q1.</div>
+				<div
+					className='ques'
+					dangerouslySetInnerHTML={{
+						__html: "What is the output of the following code?\n\nx = 5\ny = 10\nprint(x + y)",
+					}}></div>
+			</div>
+			<div className='options'>
+				<div className='option'>15</div>
+				<div className='option'>10</div>
+				<div className='option'>5</div>
+				<div className='option'>0</div>
+			</div>
 		</div>
 	);
 });
