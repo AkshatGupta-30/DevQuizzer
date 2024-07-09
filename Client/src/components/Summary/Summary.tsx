75e4794fb5a880577dc127d2083c8440a41911e0
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-09 18:06:07 +0530

    feat: seperated summary components

--- Client/src/components/Summary/Summary.tsx
+++ Client/src/components/Summary/Summary.tsx
@@ -4,90 +4,118 @@ import { QuizzContext } from "../../context/QuizContext";
 import { QuestionStatus } from "../../oops/enum/QuestionStatus";
 import Question from "../../oops/models/Question";
 
-const Summary = memo(() => {
-	const { questions, currQ, bankLength, bankPage, setBankPage } = useContext(QuizzContext);
+function getQuestionClass(status: QuestionStatus): string {
+	switch (status) {
+		case "Answered":
+			return "answered";
+		case "Mark For Review":
+			return "review";
+		case "Not Answered":
+			return "not-answered";
+		default:
+			return "not-visited";
+	}
+}
 
-	const getStatusCount = (status: QuestionStatus): number => {
-		return questions.filter((ques) => ques.questionStatus === status).length;
-	};
+const AnswerStatus = () => {
+	const { questions, currQ } = useContext(QuizzContext);
 
-	const getQuestionClass = (status: QuestionStatus): string => {
-		switch (status) {
-			case "Current":
-				return "current";
-			case "Answered":
-				return "answered";
-			case "Mark For Review":
-				return "review";
-			case "Not Answered":
-				return "not-answered";
-			default:
-				return "not-visited";
-		}
-	};
+	const getStatusCount = (status: QuestionStatus): number =>
+		questions.filter((ques) => ques.questionStatus === status).length;
 
 	return (
-		<div className='summary'>
-			<div className='answer-status'>
-				<h2>Answer Status</h2>
-				<div className='legends'>
-					{Object.values(QuestionStatus).map((status, index) => (
-						<div className='legend' key={index}>
-							<div className={`color ${getQuestionClass(status)}`}>
-								{index !== 0 ? getStatusCount(status as QuestionStatus) : null}
-							</div>
-							<label>{status}</label>
+		<div className='answer-status'>
+			<h2>Answer Status</h2>
+			<div className='legends'>
+				{Object.values(QuestionStatus).map((status, i) => (
+					<div className='legend' key={i}>
+						<div className={`color ${i === 0 ? "current" : getQuestionClass(status)}`}>
+							{i === 0 ? (currQ === -1 ? null : currQ + 1) : getStatusCount(status as QuestionStatus)}
 						</div>
-					))}
-				</div>
-			</div>
-			<div className='question-bank'>
-				<label>Question Bank</label>
-				{questions.length <= bankLength ? (
-					<div className='numbers'>
-						{questions.map((ques: Question, i: number) => {
-							return (
-								<li key={i} className={currQ === i ? "current" : getQuestionClass(ques.questionStatus)}>
-									{i + 1}
-								</li>
-							);
-						})}
+						<label>{status}</label>
 					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+const Fixedbank = () => {
+	const { questions, currQ } = useContext(QuizzContext);
+
+	return (
+		<div className='numbers'>
+			{questions.map((ques: Question, i: number) => {
+				return (
+					<li key={i} className={currQ === i ? "current" : getQuestionClass(ques.questionStatus)}>
+						{i + 1}
+					</li>
+				);
+			})}
+		</div>
+	);
+};
+
+const MoreBank = () => {
+	const { questions, currQ, bankLength, bankPage, setBankPage } = useContext(QuizzContext);
+	return (
+		<>
+			<div className='numbers blank-page'>
+				{Array.from({ length: bankLength }, (_, i) => {
+					// const quesStatus = questions[(bankPage - 1) * bankLength + i].questionStatus;
+					return questions[(bankPage - 1) * bankLength + i].questionStatus ? (
+						<li
+							key={i}
+							className={
+								currQ === i
+									? "current"
+									: getQuestionClass(questions[(bankPage - 1) * bankLength + i].questionStatus)
+							}>
+							{(bankPage - 1) * bankLength + i + 1}
+						</li>
+					) : (
+						<li key={i}></li>
+					);
+				})}
+			</div>
+			<div className='btns'>
+				{bankPage != 1 ? (
+					<button className='btn' onClick={() => setBankPage(bankPage - 1)}>
+						<div className='ico prev-ico'>➤</div>
+						<div className='name n-one'> Previous </div>
+					</button>
 				) : (
-					<>
-						<div className='numbers blank-page'>
-							{Array.from({ length: bankLength }, (_, i) => {
-								const quesStatus = questions[(bankPage - 1) * bankLength + i].questionStatus;
-								return quesStatus ? (
-									<li key={i} className={getQuestionClass(quesStatus)}>
-										{(bankPage - 1) * bankLength + i + 1}
-									</li>
-								) : (
-									<li key={i}></li>
-								);
-							})}
-						</div>
-						<div className='btns'>
-							{bankPage != 1 ? (
-								<button className='btn' onClick={() => setBankPage(bankPage - 1)}>
-									<div className='ico prev-ico'>➤</div>
-									<div className='name n-one'> Previous </div>
-								</button>
-							) : (
-								<div className='vr' style={{ background: "#f81f3f" }}></div>
-							)}
-							{bankPage != Math.ceil(questions.length / bankLength) ? (
-								<button className='btn' onClick={() => setBankPage(bankPage + 1)}>
-									<div className='name n-two'> Next </div>
-									<div className='ico next-ico'>➤</div>
-								</button>
-							) : (
-								<div className='vr' style={{ background: "#934bde" }}></div>
-							)}
-						</div>
-					</>
+					<div className='vr' style={{ background: "#f81f3f" }}></div>
+				)}
+				{bankPage != Math.ceil(questions.length / bankLength) ? (
+					<button className='btn' onClick={() => setBankPage(bankPage + 1)}>
+						<div className='name n-two'> Next </div>
+						<div className='ico next-ico'>➤</div>
+					</button>
+				) : (
+					<div className='vr' style={{ background: "#934bde" }}></div>
 				)}
 			</div>
+		</>
+	);
+};
+
+export const QuestionBank = () => {
+	const { questions, bankLength } = useContext(QuizzContext);
+
+	return (
+		<div className='question-bank'>
+			<label>Question Bank</label>
+			{questions.length <= bankLength ? <Fixedbank /> : <MoreBank />}
+		</div>
+	);
+};
+
+const Summary = memo(() => {
+	return (
+		<div className='summary'>
+			<AnswerStatus />
+			<QuestionBank />
 		</div>
 	);
 });
