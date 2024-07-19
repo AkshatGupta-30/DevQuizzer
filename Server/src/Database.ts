1bd3aea5b64ee657d690bf3e697281b8513180cd
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-19 21:04:38 +0530

    feat: Added mongodb atlas url to database

--- Server/src/Database.ts
+++ Server/src/Database.ts
@@ -1,16 +1,17 @@
-import mongoose, { Connection } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
 const DbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_LOCAL_URL!);
-        console.log("MongoDb Connected Succesfully")
-    } catch (err) {
-        console.log("Error in connecting to MongoDB server")
-        console.log(err)
-        process.exit(1)
-    }
-}
+        // await mongoose.connect(process.env.MONGO_LOCAL_URL!);
+		await mongoose.connect(process.env.MONGO_GLOBAL_URL! + "/" + process.env.DATABASE_NAME!);
+		console.log("MongoDb Connected Succesfully");
+	} catch (err) {
+		console.log("Error in connecting to MongoDB server");
+		console.log(err);
+		process.exit(1);
+	}
+};
 
-export default DbConnect
+export default DbConnect;
