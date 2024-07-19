a846255780b768383c40ddac49dd48f2172f7dfd
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-20 00:44:18 +0530

    feat: add deploy worlflow

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/devquizzers/",
 	server: {
 		port: 3000,
 		open: true,
